8a92b7c5e6e7ddc2dfc48af1ad8ce63553bcb2a6
RyanAmyElli <poststiktok20@gmail.com>
2024-06-28 22:33:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,119 +1,110 @@
 const games = [
-1. Akumu 🏚
-2. Apeirophobia 👣
-3. ABC Lore
-4. ALBERT Doors ✨✌🏻
-5. Bakon 🥓👨
-6. BEAR 🐻
-7. Before Truth 🏚
-8. Bewildered 🌲
-9. Banana Eats 🍌
-10. Cheese Escape 🧀
-11. Centipede
-12. Doe 🔪
-13. Dead Realm 🔪
-14. Dead Silence 👁
-15. Deepwoken 🎃
-16. Disturbance
-17. Dog! 😨🐶
-18. Doors 🚪👁
-19. Doors Floor 2 🚪
-20. Endless Doors 🚪
-21. Escape the Darkness 🕛🔪
-22. Eyes the Horror Game
-23. Evade
-24. ELMIRA
-25. Evelyn
-26. Eternal Insolence
-27. Frogge 🐸
-28. FNAF: Coop 🍕
-29. Faith
-30. Frigid Dusk
-31. Forsaken 🔪
-32. Floppy Playtime 🛝
-33. Gerald!
-34. Granny 👵🏽
-35. Insane Elevator 🛗
-36. Imagine
-37. Infectious Smile 😀
-38. JUDY 💅✨
-39. Little Ones 📌
-40. Minus Elevation
-41. Midnight Hours 🕛
-42. Mommy 👩‍👧
-43. Night of Noobs
-44. Nanny 🙈
-45. Nico Nextbots
-46. Panik 🦾
-47. Puppet 🧸
-48. Piggy 🐷
-49. Piggy 2 🐷
-50. Questy
-51. Ronald 🤡
-52. Rickey Rat 🐀
-53. Rainbow Friends 🌈
-54. Rainbow Friends 2 🌈
-55. Slimey
-56. Survive The Killer 🕛 🔪
-57. SCP Games 😶‍🌫️
-58. Short Creepy Stories 📚
-59. Specter
-60. School 🎃
-61. Submarine Simulator 🌊
-62. The Horror Mansion 🏠
-63. Teddy Modded 🐼
-64. Teddy Friends 🧸
-65. The Catcher 🐱
-66. The Mirror 🪞
-67. The Circus 🎪
-68. Trespass 🏚
-69. The Dead 💀
-70. The Tall Man 👨
-71. The Truth Untold
-72. The Asylum ❄️
-73. The Schoolhouse 🏫
-74. The Mimic
-75. Us Are Toys 🧸
-76. VOID
-77. Wormy 🪱
-78. 3008 🚜
-79. Nanny
-80. The Haunted Imperial Hotel
-81. Nico's Nextbots
-82. Elmira
-83. The Mimic
-84. Doors
-85. It
-86. Murder Mystery 2
-87. Flee the Facility
-88. Evade
-89. Rainbow Friends
-90. Camp Woodland
-91. The Carnival of Horrors
-92. The Creepy House
-93. The Dark Side
-94. The Forgotten Facility
-95. The Haunted Carnival
-96. The Lost Ones
-97. Ghost Hunt
-98. Apocalypse Rising
-99. Natural Disaster Survival
-100. Survive the Killer!
-101.Dress to Impress
-102.carry a friend
-103.be a duck
-104.office experience
-105.neighbors
-106.mic up
-107.Impostor
-108.horrific housing 
-109.work at a pizza place
+  "Akumu 🏚",
+  "Apeirophobia 👣",
+  "ABC Lore",
+  "ALBERT Doors ✨✌🏻",
+  "Bakon 🥓👨",
+  "BEAR 🐻",
+  "Before Truth 🏚",
+  "Bewildered 🌲",
+  "Banana Eats 🍌",
+  "Cheese Escape 🧀",
+  "Centipede",
+  "Doe 🔪",
+  "Dead Realm 🔪",
+  "Dead Silence 👁",
+  "Deepwoken 🎃",
+  "Disturbance",
+  "Dog! 😨🐶",
+  "Doors 🚪👁",
+  "Doors Floor 2 🚪",
+  "Endless Doors 🚪",
+  "Escape the Darkness 🕛🔪",
+  "Eyes the Horror Game",
+  "Evade",
+  "ELMIRA",
+  "Evelyn",
+  "Eternal Insolence",
+  "Frogge 🐸",
+  "FNAF: Coop 🍕",
+  "Faith",
+  "Frigid Dusk",
+  "Forsaken 🔪",
+  "Floppy Playtime 🛝",
+  "Gerald!",
+  "Granny 👵🏽",
+  "Insane Elevator 🛗",
+  "Imagine",
+  "Infectious Smile 😀",
+  "JUDY 💅✨",
+  "Little Ones 📌",
+  "Minus Elevation",
+  "Midnight Hours 🕛",
+  "Mommy 👩‍👧",
+  "Night of Noobs",
+  "Nanny 🙈",
+  "Nico Nextbots",
+  "Panik 🦾",
+  "Puppet 🧸",
+  "Piggy 🐷",
+  "Piggy 2 🐷",
+  "Questy",
+  "Ronald 🤡",
+  "Rickey Rat 🐀",
+  "Rainbow Friends 🌈",
+  "Rainbow Friends 2 🌈",
+  "Slimey",
+  "Survive The Killer 🕛 🔪",
+  "SCP Games 😶‍🌫️",
+  "Short Creepy Stories 📚",
+  "Specter",
+  "School 🎃",
+  "Submarine Simulator 🌊",
+  "The Horror Mansion 🏠",
+  "Teddy Modded 🐼",
+  "Teddy Friends 🧸",
+  "The Catcher 🐱",
+  "The Mirror 🪞",
+  "The Circus 🎪",
+  "Trespass 🏚",
+  "The Dead 💀",
+  "The Tall Man 👨",
+  "The Truth Untold",
+  "The Asylum ❄️",
+  "The Schoolhouse 🏫",
+  "The Mimic",
+  "Us Are Toys 🧸",
+  "VOID",
+  "Wormy 🪱",
+  "3008 🚜",
+  "Nanny",
+  "The Haunted Imperial Hotel",
+  "Nico's Nextbots",
+  "Elmira",
+  "The Mimic",
+  "Doors",
+  "It",
+  "Murder Mystery 2",
+  "Flee the Facility",
+  "Evade",
+  "Rainbow Friends",
+  "Camp Woodland",
+  "The Carnival of Horrors",
+  "The Creepy House",
+  "The Dark Side",
+  "The Forgotten Facility",
+  "The Haunted Carnival",
+  "The Lost Ones",
+  "Ghost Hunt",
+  "Apocalypse Rising",
+  "Natural Disaster Survival",
+  "Survive the Killer!",
+  "Dress to Impress",
+  "carry a friend",
+  "be a duck"
 ];
 
-const pickGameBtn = document.getElementById("pick-game-btn");
-
-pickGameBtn.addEventListener("click", () => {
-  const randomIndex = Math.floor(Math.random() * games.length);
-  const pickedGame = games[randomIndex];
-  alert(`You picked: ${pickedGame}!`);
+document.getElementById("pick-game-btn").addEventListener("click", function() {
+  const randomGame = games[Math.floor(Math.random() * games.length)];
+  alert(`You should play ${randomGame}!`);
 });
